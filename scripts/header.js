503f7b201c1a00eb8bcee2fd62fb2801ca07373f
Bashir, Amir Kano <99528741+Bashir76@users.noreply.github.com>
2023-03-11 12:00:21 +0100

    Create header.js

---
+++ scripts/header.js
@@ -0,0 +1,14 @@
+/* this file contain headers js code */
+
+function myTab(x) {
+  x.classList.toggle("change");
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
